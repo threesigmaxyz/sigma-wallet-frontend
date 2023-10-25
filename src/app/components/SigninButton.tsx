9dfe22a0a230d1bb35518d00257d806965eaecf1
0x73696d616f <116109421+0x73696d616f@users.noreply.github.com>
2023-10-25 12:57:14 +0100

    chore: text box frontend

--- src/app/components/SigninButton.tsx
+++ src/app/components/SigninButton.tsx
@@ -7,22 +7,27 @@ const SigninButton = () => {
   const { data: session } = useSession();
   const [nonce, setNonce] = useState("XXX");
 
+  const getToken = async (provider : any) => {
+    signIn(provider, {}, { nonce: nonce });
+  }
+
   if (session && session.user) {
     return (
       <div className="flex gap-4 ml-auto">
         <input style={{ color: 'black' }} type="text" value={nonce} onChange={(e) => setNonce(e.target.value)} />
-        <button onClick={() => signIn('google', {}, { nonce: nonce })}>Update Google</button>
-        <button onClick={() => signIn('facebook', {}, { nonce: nonce })}>Update Facebook</button>
-        <button onClick={() => signIn('azure-ad', {}, { nonce: nonce })}>Update Microsoft</button>
+        <button onClick={() => getToken('google')}>Update Google</button>
+        <button onClick={() => getToken('facebook')}>Update Facebook</button>
+        <button onClick={() => getToken('azure-ad')}>Update Microsoft</button>
         <p className="text-sky-600">{session.user.name}</p>
         <button onClick={() => signOut()} className="text-red-600">
           Sign Out
         </button>
+        <textarea className="text-sky-600" value={session?.id_token} />
       </div>
     );
   }
   return (
-    <button onClick={() => signIn()} className="text-green-600 ml-auto">
+    <button onClick={() => getToken('any provider')} className="text-green-600 ml-auto">
       Sign In
     </button>
   );
